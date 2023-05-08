b50de3968ac8918fb3a6383696fec61535bb090a
Mathieuka <mathieu.kahlaoui.pro@gmail.com>
2023-05-08 17:24:02 +0200

    algoexpert(longestPeak): final implementation

--- src/__tests__/longestPeak.test.ts
+++ src/__tests__/longestPeak.test.ts
@@ -1,23 +1,32 @@
 import { longestPeak } from "@/algorithmic/longestPeak";
 
 describe("Longest Peak", () => {
-  it("[length 3] Should find a long peak of 3 points", () => {
+  it("Should find a long peak of 3 points", () => {
     expect(longestPeak([0, 1, 0])).toEqual(3);
   });
 
-  it("[length 4] Should find a long peak of 3 points", () => {
+  it("Should find a long peak of 3 points", () => {
     expect(longestPeak([1, 0, 1, 0])).toEqual(3);
   });
 
-  it("[length 4] Should not find peak", () => {
+  it("Should not find peak", () => {
     expect(longestPeak([1, 2, 3, 3, 4])).toEqual(0);
   });
 
-  it("[length 6] Should find a long peak of 6 points", () => {
+  it("Should find a long peak of 6 points", () => {
     expect(longestPeak([1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3])).toEqual(6);
   });
 
-  it("[length 6] Should find a long peak of 5 points", () => {
+  it("Should find a long peak of 5 points", () => {
     expect(longestPeak([1, 2, 3, 2, 1, 1])).toEqual(5);
+  });
+
+  it("Should find a long peak of 9 points", () => {
+    expect(
+      longestPeak([
+        1, 1, 1, 2, 3, 10, 12, -3, -3, 2, 3, 45, 800, 99, 98, 0, -1, -1, 2, 3,
+        4, 5, 0, -1, -1,
+      ])
+    ).toEqual(9);
   });
 });
--- src/algorithmic/longestPeak.ts
+++ src/algorithmic/longestPeak.ts
@@ -1,42 +1,43 @@
-const updateLongestPeak = (longestPeak: number, accumulator: number) =>
-  Math.max(longestPeak, accumulator);
+const isAPeak = ({
+  prev,
+  curr,
+  next,
+}: {
+  prev: number;
+  curr: number;
+  next: number;
+}) => curr > prev && curr > next;
 
 export const longestPeak = (arr: number[]): number => {
-  // iterate on numbers with while loop until index equal array length
-  // check if is a peak
-  // iterate on left side and count
-  // iterate on right side and count
-  // update index
-
-  let i = 0;
-  let iCount = 0;
-  const isAPeak = ({
-    prev,
-    curr,
-    next,
-  }: {
-    prev: number;
-    curr: number;
-    next: number;
-  }) => curr > prev && curr > next;
+  let maxPeakLength = 0;
   let counter = 0;
+  let i = 0;
+  let pointIndex = 0;
+
+  let currentPointIsGreaterThenThePrevious = (pointIndex: number) =>
+    arr[pointIndex] > arr[pointIndex - 1];
+  let currentPointIsGreaterThenTheNext = (pointIndex: number) =>
+    arr[pointIndex] > arr[pointIndex + 1];
+
   while (i < arr.length) {
     i++;
     if (isAPeak({ prev: arr[i - 1], curr: arr[i], next: arr[i + 1] })) {
-      counter++;
+      counter = 1;
+      pointIndex = i;
 
-      if (arr[i] > arr[i - 1]) {
+      while (currentPointIsGreaterThenThePrevious(pointIndex)) {
+        pointIndex = pointIndex - 1;
         counter++;
       }
 
-      if (arr[i] > arr[i + 1]) {
+      pointIndex = i;
+      while (currentPointIsGreaterThenTheNext(pointIndex)) {
+        pointIndex = pointIndex + 1;
         counter++;
       }
 
-      // while (arr[i] > arr[i - 1]) {
-      //
-      // }
+      maxPeakLength = Math.max(maxPeakLength, counter);
     }
   }
-  return counter;
+  return maxPeakLength;
 };
